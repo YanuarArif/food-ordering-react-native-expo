e6311eb25d7d734e1fc6f43a181ca3615655b649
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2024-06-12 22:05:24 +0700

    menambah cart modal, tapi di android modal tidak didukung (only iOS)

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,7 +1,27 @@
-import { Stack } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+import { Link, Stack } from "expo-router";
+import { Pressable } from "react-native";
 
 export default function MenuLayout() {
   return (
-    <Stack screenOptions={{ title: "Menu", headerTitleAlign: "center" }} />
+    <Stack
+      screenOptions={{
+        title: "Menu",
+        headerTitleAlign: "center",
+        headerRight: () => (
+          <Link href="/cart" asChild>
+            <Pressable>
+              <FontAwesome
+                name="shopping-cart"
+                size={20}
+                color={Colors.light.tint}
+                style={{ marginRight: 15 }}
+              />
+            </Pressable>
+          </Link>
+        ),
+      }}
+    />
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,10 @@ import { Stack } from "expo-router";
 
 export default function _layout() {
   return (
-    <Stack screenOptions={{ headerShown: false, statusBarColor: "black" }} />
+    <Stack screenOptions={{ headerShown: true, statusBarColor: "black" }}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* presentation: "modal" tidak berfungsi pada android */}
+      <Stack.Screen name="cart" options={{ presentation: "modal" }} />
+    </Stack>
   );
 }
